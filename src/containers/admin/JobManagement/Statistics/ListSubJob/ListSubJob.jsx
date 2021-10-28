9f0cab6721a464ac56533be76d728a6b7c63b3a6
NhatNamVo <nhatnamvstt@gmail.com>
2021-10-28 23:21:46 +0700

    completed sub job type management

--- src/containers/admin/JobManagement/Statistics/ListSubJob/ListSubJob.jsx
+++ src/containers/admin/JobManagement/Statistics/ListSubJob/ListSubJob.jsx
@@ -3,34 +3,124 @@ import {useSelector, useDispatch} from 'react-redux';
 import { Form, Input, Button, Space } from "antd";
 import {SettingOutlined, DeleteOutlined, AppstoreAddOutlined, CheckCircleOutlined, CloseCircleOutlined} from '@ant-design/icons';
 import './ListSubJob.scss';
+import { actAddNewSubJobType, actDeleteSubJobType, actUpdateSubJob } from 'Modules/JobManagement/actions';
+
 const Listsubjob = (props) => {
-    const [titleMainJob, setTitleMainJob] = useState('');
     const formRef = React.createRef();
     const [isAdd, setIsAddNew] = useState(false);
+    const [isEdit, setIsEdit] = useState(false);
+    const [editState, setEditState] = useState(null);
     const {jobId} = props;
-    const { mainJob } = useSelector((state) => state.JobManagementReducer);
+    const { mainJob,loadingAction, isError} = useSelector((state) => state.JobManagementReducer);
+    const dispatch = useDispatch();
     const addNewSubJob = (e) => {
         if(!isAdd){
             setIsAddNew(true);
+            document.querySelector('.subJobType__title input').focus();
         }
     }
     const approveAddNew = (e) => {
-        console.log('addnew')
+        document.querySelector('#submidAddSub').click();
     }
     const cancleAddNew = (e) => {
         setIsAddNew(false);
+        formRef.current.resetFields();
     }
     const onFinish = (values) => {
-        const data = {...values, status:true};
+        const data = {...values, status:true, typeJob: mainJob[jobId]?._id};
+        dispatch(actAddNewSubJobType(data));
     };
-    // const addNew = (e) => {
-    //     // const approveBtn = e.target.closest('span:nth-of-type(1)');
-    //     // const cancleBtn = e.target.closest('span:nth-of-type(2)');
-    //     // if(!!cancleBtn){
-    //         setIsAdd(false);
-    //     //     console.log(isAdd);
-    //     // }
-    // }
+    const editFunc = (e) => {
+        const subJobTypeBox = document.querySelectorAll('.subJobType-item');
+        const key = e.target.closest('.subJobType-item').dataset.key;
+        const subJobId = e.target.closest('.subJobType-item').dataset.subjobid;
+        const subJobItemSelected = subJobTypeBox[key].querySelector('.subJobType__title');
+        const subJobItemEditing = subJobTypeBox[key].querySelector('.subJobType__settingContent');
+        const prevContent = subJobItemSelected.innerText;
+        subJobItemSelected.setAttribute('contentEditable',true);
+        subJobItemSelected.focus();
+        subJobItemEditing.classList.add('isEdit');
+        let edit = {
+            key: null,
+            idJob: '',
+            prevContent: '',
+        };
+        edit.key = key;
+        edit.idJob = subJobId;
+        edit.prevContent = prevContent;
+        setEditState(edit)
+        setIsEdit(true);
+    }
+    const deleteFunc = (e) => {
+        const subJobTypeBox = document.querySelectorAll('.subJobType-item');
+        const key = e.target.closest('.subJobType-item').dataset.key;
+        const subJobId = e.target.closest('.subJobType-item').dataset.subjobid;
+        const subJobItemSelected = subJobTypeBox[key].querySelector('.subJobType__title');
+        const subJobItemEditing = subJobTypeBox[key].querySelector('.subJobType__settingContent');
+        subJobItemSelected.classList.add('remove');
+        subJobItemEditing.classList.add('isEdit');
+        let edit = {
+            key: null,
+            idJob: '',
+            prevContent: '',
+        };
+        edit.key = key;
+        edit.idJob = subJobId;
+        setEditState(edit);
+        setIsEdit(false);
+    }
+    const approveSubJob = (e) => {
+        const subJobTypeBox = document.querySelectorAll('.subJobType-item');
+        const subJobItemSelected = subJobTypeBox[editState.key].querySelector('.subJobType__title');
+        const subJobItemEditing = subJobTypeBox[editState.key].querySelector('.subJobType__settingContent');
+        const currentContent = subJobItemSelected.innerText;
+        if(!!isEdit){
+            if(!!currentContent){
+                if(currentContent !== editState.prevContent){
+                    const data = {name:currentContent,status: true, typeJob: mainJob[jobId]?._id};
+                    dispatch(actUpdateSubJob(editState.idJob,data))
+                }  
+                else{
+                    subJobItemSelected.innerHTML = editState.prevContent;
+                }
+                subJobItemEditing.classList.remove('isEdit');
+                subJobItemSelected.classList.remove('error');
+                subJobItemSelected.setAttribute('contentEditable',false);
+            }
+            else{
+                subJobItemSelected.classList.add('error');
+                subJobItemSelected.focus();
+            }
+        }
+        else{
+            dispatch(actDeleteSubJobType(editState.idJob));
+            subJobItemEditing.classList.remove('isEdit');
+        }
+       
+    }
+    const cancleSubJob = () => {
+        const subJobTypeBox = document.querySelectorAll('.subJobType-item');
+        const subJobItemSelected = subJobTypeBox[editState.key].querySelector('.subJobType__title');
+        const subJobItemEditing = subJobTypeBox[editState.key].querySelector('.subJobType__settingContent');
+        if(!!isEdit){
+            subJobItemEditing.classList.remove('isEdit');
+            subJobItemSelected.classList.remove('error');
+            subJobItemSelected.innerHTML = editState.prevContent;
+            subJobItemSelected.setAttribute('contentEditable',false);
+            setEditState(null);
+            setIsEdit(false);
+        }
+        else{
+            subJobItemEditing.classList.remove('isEdit');
+            subJobItemSelected.classList.remove('remove');
+            setEditState(null);
+        }
+    }
+    const resetValue = () => {
+        const subJobTypeBox = document.querySelectorAll('.subJobType-item');
+        const subJobItemSelected = subJobTypeBox[editState?.key].querySelector('.subJobType__title');
+        subJobItemSelected.innerHTML = editState.prevContent;
+    }
     const renderAddSubJob = () => {
         if(isAdd){
             return (
@@ -44,12 +134,22 @@ const Listsubjob = (props) => {
             return <AppstoreAddOutlined />
         }
     }
-    // useEffect(()=>{
-    //     setTitleMainJob(mainJob[jobId]?.name);
-    // },[])
-    // useEffect(()=>{
-    //     setTitleMainJob(mainJob[jobId]?.name);
-    // },[jobId])
+    useEffect(()=>{
+        if(!!isError){
+            if(!!isEdit){
+                resetValue();
+                setEditState(null);
+                setIsEdit(false);
+            }
+            else{
+                setEditState(null);
+            }
+        }
+        else{
+            setEditState(null);
+        }
+        cancleAddNew();
+    },[loadingAction])
     return (
         <div className='subTypeJob-content'>
             <div className="JobType-title">
@@ -60,11 +160,13 @@ const Listsubjob = (props) => {
                 <div className="subJobType-content row">
                     {mainJob[jobId]?.subTypeJobs.map((subJobType,idx)=>{
                         return (
-                            <div key={idx} data-id={idx} data-subjobid = {subJobType._id} className="subJobType-item col-md-3">
+                            <div key={idx} data-key={idx} data-subjobid = {subJobType._id} className={"subJobType-item col-md-3 " + (!editState||editState?.key == idx?"":"disabled")}>
                                 <div className="subJobType__content row">
                                     <div className="subJobType__settingContent">
-                                        <SettingOutlined />
-                                        <DeleteOutlined />
+                                        <SettingOutlined onClick={editFunc}/>
+                                        <DeleteOutlined onClick={deleteFunc}/>
+                                        <CheckCircleOutlined onClick = {approveSubJob}/>
+                                        <CloseCircleOutlined onClick = {cancleSubJob}/>
                                     </div>
                                     <div className="subJobType__title">
                                         {subJobType.name}
@@ -73,13 +175,13 @@ const Listsubjob = (props) => {
                             </div>
                         )
                     })}
-                    <div className="subJobType-item col-md-3">
+                    <div className={"subJobType-item col-md-3 " + (!editState?"":"disabled")}>
                         <div className={"subJobType__content subJobType-add row " + (isAdd?'addnew':'')} onClick={addNewSubJob}>
                         <div className="subJobType__settingContent">
                             {renderAddSubJob()}
                         </div>
                         <div className="subJobType__title">
-                            Add new sub job
+                            <p>Add new sub job</p>
                             <Form
                                 onFinish={onFinish}
                                 ref={formRef}
@@ -96,7 +198,7 @@ const Listsubjob = (props) => {
                                     validator(_, value) {
                                         let isDublicate = -1;
                                         if(value){
-                                        isDublicate = mainJob.findIndex(job=>{
+                                        isDublicate = mainJob[jobId]?.subTypeJobs.findIndex(job=>{
                                             return job.name.toLowerCase() === value.toLowerCase();
                                         });
                                         }
@@ -108,10 +210,10 @@ const Listsubjob = (props) => {
                                     }),
                                 ]}
                                 >
-                                <Input />
+                                <Input placeholder="Add new sub job"/>
                                 </Form.Item>
-                                <Button style={{visibility: 'hidden'}} type="primary" id="submidAddNew" htmlType="submit">
-                                Register
+                                <Button style={{visibility: 'hidden'}} type="primary" id="submidAddSub" htmlType="submit">
+                                    ...
                                 </Button>
                             </Form>
                         </div>
