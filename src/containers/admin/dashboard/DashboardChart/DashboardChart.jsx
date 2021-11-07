50a596cefb962e31957e1ff7f9487f896131a56a
Vo Nhat Nam <nhatnamvstt@gmail.com>
2021-11-07 23:10:18 +0700

    complete dashboard

---
+++ src/containers/admin/dashboard/DashboardChart/DashboardChart.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import ReactECharts from "echarts-for-react";
+import './DashboardChart.scss';
+const Dashboardchart = () => {
+  const [dataChart, setDataChart] = useState([]);
+  const { userSatictis } = useSelector((state) => state.managementUserReducer);
+  const { dataSatictis } = useSelector((state) => state.JobManagementReducer);
+  useEffect(() => {
+    const data = [];
+    const jobQty = dataSatictis.reduce((qty, job) => {
+      return qty + job.jobQty;
+    }, 0);
+    const clientQty = userSatictis.length;
+    data.push(jobQty, clientQty);
+    setDataChart(data);
+  }, [userSatictis,dataSatictis]);
+  const optionsBarChart = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "shadow",
+      },
+    },
+    grid: {
+      left: "center",
+      top: "15%",
+      bottom: "6%",
+      containLabel: true,
+      width: "70%",
+    },
+    xAxis: {
+      type: "category",
+      data: ["Job", "User"],
+      //   axisLabel: {
+      //     interval: 0,
+      //     rotate: 45,
+      //   },
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+        {
+          name: "Quality",
+          type: "bar",
+          barWidth: "50%",
+          data: dataChart,
+          itemStyle: {
+            color: "#73BFDD"
+          }
+        },
+      ],
+    animation: true,
+  };
+  return (
+    <div className="dashboardChart">
+      <div className="dashboardChart__content">
+        <ReactECharts
+          option={{
+            ...optionsBarChart,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboardchart;
