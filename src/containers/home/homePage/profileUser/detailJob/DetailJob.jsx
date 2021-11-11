792c075ea611b5a24ed8af31c4c2397a201af806
thuanminh <thuanminh304@gmail.com>
2021-11-11 13:38:49 +0700

    done-update-job

--- src/containers/home/homePage/profileUser/detailJob/DetailJob.jsx
+++ src/containers/home/homePage/profileUser/detailJob/DetailJob.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import { actGetDetailJobCreatedByUser } from "../createNewJobByUser/StepsCreateNewGig/modules/action";
 import "./detailJob.scss";
-import { Anchor } from "antd";
-
+import { FieldTimeOutlined } from "@ant-design/icons";
 export default function DetailJob() {
   const params = useParams();
   const dispatch = useDispatch();
@@ -17,27 +17,70 @@ export default function DetailJob() {
   useEffect(() => {
     dispatch(actGetDetailJobCreatedByUser(params?.idJobCreateByUser));
   }, []);
-  var barFixed = document.getElementById("bar-fixed");
-//   var fixed = barFixed.offsetTop;
-//   window.addEventListener("scroll", function () {
-//     if (window.pageYOffset >= fixed) {
-//       fixed.classList.add("fixed");
-//     } else {
-//       fixed.classList.remove("fixed");
-//     }
-//   });
+  const [over, setOver] = useState(false);
+  const [seller, setSeller] = useState(false);
+  const clickOver = () => {
+    setOver(() => {
+      return true;
+    });
+    setSeller(() => {
+      return false;
+    });
+  };
+  if (over) {
+    document.querySelector(".over").style.borderBottom = "4px solid #1dbf73";
+    document.querySelector(".seller").style.borderBottom = "none";
+  }
+  const clickSeller = () => {
+    setOver(() => {
+      return false;
+    });
+    setSeller(() => {
+      return true;
+    });
+  };
+  if (seller) {
+    document.querySelector(".seller").style.borderBottom = "4px solid #1dbf73";
+    document.querySelector(".over").style.borderBottom = "none";
+  }
+  console.log(over, seller);
+
   return (
     <div className="detail-job">
       <div className="detail-job-content">
-        <div id="bar-fixed">
+        <div className="bar-fixed navbar sticky-top">
           <ul>
             <li>
-              <a href="#overview">Overview</a>
+              <a
+                className="over"
+                onClick={() => {
+                  clickOver();
+                }}
+                href="#overview"
+              >
+                Overview
+              </a>
             </li>
             <li>
-              <a href="#abouttheseller">About The Seller</a>
+              <a
+                className="seller"
+                onClick={() => {
+                  clickSeller();
+                }}
+                href="#abouttheseller"
+              >
+                About The Seller
+              </a>
             </li>
           </ul>
+
+          <button>
+            {" "}
+            <Link to={`/by-user/update-job/${detailJobCreatedByUser?._id}`}>
+              <i class="fa fa-edit"></i> Edit Gig
+            </Link>
+            <span></span>
+          </button>
         </div>
         <div className="share-gig">
           <div className="content">
@@ -100,7 +143,141 @@ export default function DetailJob() {
               </div>
             </div>
           </div>
-          <div className="col-5"></div>
+          <div className="col-4 ">
+            <div className="main-content-right sticky-top">
+              <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                  <a
+                    class="nav-link active"
+                    id="home-tab"
+                    data-toggle="tab"
+                    href="#home"
+                    role="tab"
+                    aria-controls="home"
+                    aria-selected="true"
+                  >
+                    Basic
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link"
+                    id="profile-tab"
+                    data-toggle="tab"
+                    href="#profile"
+                    role="tab"
+                    aria-controls="profile"
+                    aria-selected="false"
+                  >
+                    Standard
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link"
+                    id="contact-tab"
+                    data-toggle="tab"
+                    href="#contact"
+                    role="tab"
+                    aria-controls="contact"
+                    aria-selected="false"
+                  >
+                    Premium
+                  </a>
+                </li>
+              </ul>
+              <div class="tab-content" id="myTabContent">
+                <div
+                  class="tab-pane fade show active"
+                  id="home"
+                  role="tabpanel"
+                  aria-labelledby="home-tab"
+                >
+                  <div className="tab-pane-content">
+                    <div>
+                      <p>
+                        {" "}
+                        <FieldTimeOutlined />3 Days Delivery
+                      </p>
+                      <ul>
+                        <li>
+                          <i class="fa fa-check"></i>Commercial Use
+                        </li>
+                        <li>
+                          <i class="fa fa-check"></i>Source File
+                        </li>
+                        <li>
+                          <i class="fa fa-check"></i>High Resolution
+                        </li>
+                      </ul>
+                    </div>
+                    <p>US${detailJobCreatedByUser?.price}</p>
+                  </div>
+                  <button>Continue (US${detailJobCreatedByUser?.price})</button>
+                </div>
+                <div
+                  class="tab-pane fade"
+                  id="profile"
+                  role="tabpanel"
+                  aria-labelledby="profile-tab"
+                >
+                  <div className="tab-pane-content">
+                    <div>
+                      <p>
+                        {" "}
+                        <FieldTimeOutlined />3 Days Delivery
+                      </p>
+                      <ul>
+                        <li>
+                          <i class="fa fa-check"></i>Commercial Use
+                        </li>
+                        <li>
+                          <i class="fa fa-check"></i>Source File
+                        </li>
+                        <li>
+                          <i class="fa fa-check"></i>High Resolution
+                        </li>
+                      </ul>
+                    </div>
+                    <p>US${detailJobCreatedByUser?.price + 15}</p>
+                  </div>
+                  <button>
+                    Continue (US${detailJobCreatedByUser?.price + 15})
+                  </button>
+                </div>
+                <div
+                  class="tab-pane fade"
+                  id="contact"
+                  role="tabpanel"
+                  aria-labelledby="contact-tab"
+                >
+                  <div className="tab-pane-content">
+                    <div>
+                      <p>
+                        {" "}
+                        <FieldTimeOutlined />3 Days Delivery
+                      </p>
+                      <ul>
+                        <li>
+                          <i class="fa fa-check"></i>Commercial Use
+                        </li>
+                        <li>
+                          <i class="fa fa-check"></i>Source File
+                        </li>
+                        <li>
+                          <i class="fa fa-check"></i>High Resolution
+                        </li>
+                      </ul>
+                    </div>
+                    <p>US${detailJobCreatedByUser?.price + 30}</p>
+                  </div>
+                  <button>
+                    Continue (US${detailJobCreatedByUser?.price + 30})
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
